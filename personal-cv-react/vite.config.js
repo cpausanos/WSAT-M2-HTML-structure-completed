92a5eb596e9b41fe4326e9e40cc7f9adce02b261
cpausanos <pausanoschristianpaul033@gmail.com>
2026-02-27 14:37:19 +0800

    WSAT-M5: Migrated CV to React

--- personal-cv-react/vite.config.js
+++ personal-cv-react/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/WSAT-M2-HTML-structure-completed/personal-cv-react/',
+  base: '/WSAT-M2-HTML-structure-completed/',
 })
